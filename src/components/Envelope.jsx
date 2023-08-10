e5d666b82b0553fbdfa2cbf0893fd5372d469b56
JenTJames <jenthomasjames@gmail.com>
2023-08-10 23:33:38 +0530

    Feat: Integrate with backend to get inbox of user

--- src/components/Envelope.jsx
+++ src/components/Envelope.jsx
@@ -2,24 +2,24 @@ import { Typography } from "@mui/material";
 
 import Avatar from "./Avatar";
 
-const Envelope = () => {
+const Envelope = ({ mail }) => {
   return (
     <div className="flex items-center w-full gap-3 hover:bg-emerald-100 cursor-pointer p-2">
       <Avatar variant="circle" bgColor="orange" />
       <div className="flex flex-col w-full">
         <div className="w-full flex justify-between">
           <Typography variant="p" className="text-slate-500">
-            Puma
+            {mail.sender.name}
           </Typography>
           <Typography variant="p" className="text-slate-500">
             10:30pm
           </Typography>
         </div>
         <Typography variant="h6" className="text-slate-700">
-          Order Placed!
+          {mail.subject}
         </Typography>
         <Typography variant="p" className="text-slate-500">
-          Thank you for placing the order!...
+          {mail.body}
         </Typography>
       </div>
     </div>
--- src/components/EnvelopeContainer.jsx
+++ src/components/EnvelopeContainer.jsx
@@ -1,28 +1,61 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+
+import useHttp from "../hooks/use-http";
 
 import EnvelopeContainerDetails from "./EnvelopeContainerDetails";
 import Input from "./Input";
 import Envelope from "./Envelope";
+import Spinner from "./Spinner";
+import Toast from "./Toast";
 
 const EnvelopeContainer = () => {
+  const [mails, setMails] = useState([]);
+
+  const location = useLocation();
+
+  const {
+    fireRequest: fetchMails,
+    isLoading: isFetchingMails,
+    error: fetchMailsError,
+    resetError: resetFetchMailsError,
+  } = useHttp();
+
+  useEffect(() => {
+    const getMails = async () => {
+      const endpoint = "mails/" + location.pathname.split("/")[1] + "/users/2";
+      const response = await fetchMails("GET", endpoint);
+      if (!response) return;
+      setMails(response.data);
+    };
+    getMails();
+  }, [location, fetchMails]);
+
   return (
-    <div className="w-[400px] border h-full flex flex-col gap-3 p-5">
-      <EnvelopeContainerDetails />
-      <Input
-        variant="filled"
-        startAdornment={<SearchRoundedIcon />}
-        config={{
-          placeholder: "Search",
-        }}
-      />
-      <div className="flex flex-col">
-        <Envelope />
-        <Envelope />
-        <Envelope />
-        <Envelope />
-        <Envelope />
+    <>
+      {isFetchingMails && <Spinner />}
+      {fetchMailsError?.isError && (
+        <Toast variant="error" updateError={resetFetchMailsError}>
+          {fetchMailsError?.error}
+        </Toast>
+      )}
+      <div className="w-[400px] border h-full flex flex-col gap-3 p-5">
+        <EnvelopeContainerDetails />
+        <Input
+          variant="filled"
+          startAdornment={<SearchRoundedIcon />}
+          config={{
+            placeholder: "Search",
+          }}
+        />
+        <div className="flex flex-col">
+          {mails.map((mail) => (
+            <Envelope mail={mail} key={mail.id} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
